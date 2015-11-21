a913e44ab181718e1969277049d92489033b6533
piranon <piranon.n@gmail.com>
2015-11-21 17:32:15 +0700

    Clean unuse files on customer group

--- root_modules/com_gae_user_customer_group/v1_0_0/app/controllers/start.php
+++ root_modules/com_gae_user_customer_group/v1_0_0/app/controllers/start.php
@@ -1,53 +1,9 @@
-<?
-	//start file
-class start extends base_module_controller{
-	
+<?php
 
-    public function index(){
-        $this->load->model("module_model");
-        $this->mLoadView("start/example",array("myData"=>"This is MyData"));
-        $module_array = $this->module_model->getAllModuleList();
+class start extends base_module_controller
+{
+    public function index()
+    {
         return;
     }
-
-    public function test(){
-        $this->mLoadLibrary("my_lib1");
-
-        $callString = $this->my_lib1->getCallString();
-        $content = $this->my_lib1->getContent();
-        
-        $viewData["callString"] = $callString;
-        $viewData["content"] = $content;
-
-        $this->mLoadView("start/content",$viewData);
-
-        $car_spec_lib = $this->mLoadLibrary("car_spec_lib",true);
-    
-    }
-
-    public function test_imageupload(){
-
-        $viewData = array();
-        $this->mLoadView("start/test_imageupload",$viewData);
-
-    }
-
-    public function single_imageupload(){
-
-        $viewData = array();
-        $this->mLoadView("start/single_imageupload",$viewData);
-
-    }
-
-     public function multiple_imageupload(){
-
-        $viewData = array();
-        $this->mLoadView("start/multiple_imageupload",$viewData);
-
-    }
-
-
-
 }
-
-?>
