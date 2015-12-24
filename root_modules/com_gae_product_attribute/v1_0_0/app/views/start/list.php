910b097ad0144b9eed04d8303c232547801cbe95
piranon <piranon.n@gmail.com>
2015-12-24 15:43:04 +0700

    Create attribute

---
+++ root_modules/com_gae_product_attribute/v1_0_0/app/views/start/list.php
@@ -0,0 +1,308 @@
+<div ng-app="category" ng-controller="ListController as list">
+    <div class="top-navigation">
+        <div class="row module-container">
+            <div class="col-md-4"></div>
+            <div class="col-md-4 topic-page">Categories</div>
+        </div>
+    </div>
+    <div class="box-search">
+        <div class="module-container">
+            <input type="text" ng-model="list.search" class="new-search-btn" placeholder="Search Here">
+        </div>
+    </div>
+    <div class="customer-list">
+        <div class="module-container">
+            <div class="row content-header">
+                <div class="col-sm-12">
+                    Create Main Category
+                </div>
+            </div>
+            <div class="row content-desc">
+                <div class="col-sm-12">
+                    สร้างหมวดสินค้าหลัก
+                </div>
+            </div>
+            <div class="create-category" ng-show="!list.displaySubCateForm">
+                <div>
+                    <div class="create-category-label">Category Name</div>
+                    <div class="create-category-desc">ชื่อหมวดสินค้า</div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" ng-model="list.categoryName" id="category_name"
+                               ng-keydown="list.keyDownRequired($event)">
+
+                        <div class="add-warning hide" ng-hide='list.categoryName'></div>
+                    </div>
+                </div>
+                <div>Icon</div>
+                <div>
+                    <div ng-click="list.clickOnUpload(); $event.stopPropagation();">
+                        <div class="area-inner-container">
+                            <img id="area-inner-image" src="{{list.imageProfile}}">
+                        </div>
+                        <span ng-hide="list.imageProfile" id="pic-icon"></span>
+                    </div>
+                    <div class="hide">
+                        <input type="file" file-model='list.fileModel' id="imageCategory" onchange="PreviewImage();">
+                    </div>
+                </div>
+                <div>
+                    <div class="create-category-label">Label Color</div>
+                    <div class="create-category-desc">สีพื้นป้าย</div>
+                </div>
+                <div>
+                    <script type="text/javascript">
+                        $(document).ready(function () {
+                            $("#colorPicker").tinycolorpicker();
+                            $("#colorPicker2").tinycolorpicker();
+                        });
+                    </script>
+                    <div id="colorPicker" class="colorPicker">
+                        <a class="color">
+                            <div class="colorInner"></div>
+                        </a>
+
+                        <div class="track" ng-click="list.colorPick('#colorPicker');"></div>
+                        <ul class="dropdown">
+                            <li></li>
+                        </ul>
+                        <input type="hidden" class="colorInput" id="labelColor">
+                    </div>
+                </div>
+                <div>
+                    <div class="create-category-label">Font Color</div>
+                    <div class="create-category-desc">สีตัวอักษร</div>
+                </div>
+                <div>
+                    <div id="colorPicker2" class="colorPicker">
+                        <a class="color">
+                            <div class="colorInner"></div>
+                        </a>
+
+                        <div class="track"></div>
+                        <ul class="dropdown">
+                            <li></li>
+                        </ul>
+                        <input type="hidden" class="colorInput" id="fontColor">
+                    </div>
+                </div>
+                <div>
+                    <a class="btn-add" ng-click="list.addCategory(false);">
+                        <span></span>
+                    </a>
+                </div>
+            </div>
+            <div class="create-category create-sub-category" ng-show="list.displaySubCateForm">
+                <div>
+                    <div class="create-category-label">Category Name</div>
+                    <div class="create-category-desc">ชื่อหมวดสินค้า</div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" ng-model="list.subCategoryName" id="category_name"
+                               ng-keydown="list.keyDownRequired($event)" placeholder="{{list.placeholderSubCateName}}">
+
+                        <div class="add-warning hide" ng-hide='list.subCategoryName'></div>
+                    </div>
+                </div>
+                <div>
+                    <a class="btn-add" ng-click="list.addCategory(true);">
+                        <span></span>
+                    </a>
+                </div>
+            </div>
+            <div class="list-filter">
+                <div>
+                    <select ng-model="list.bulkDelete" ng-change="list.onChangeBulkDelete(list.bulkDelete)"
+                            class="form-control ng-pristine ng-valid ng-touched bulk-delete">
+                        <option value="">Bulk Action</option>
+                        <option value="1">Show Selected</option>
+                        <option value="2">Hide Selected</option>
+                        <option value="3">Delete Selected</option>
+                    </select>
+                </div>
+                <div>&nbsp;
+                    <div ng-show="list.items">
+                        {{list.selectedDeleteId.length}} category selected from total {{list.total}} categories
+                    </div>
+                </div>
+                <div>
+                    <select ng-model="list.limitList" ng-change="list.onChangeLimit(limitList)"
+                            class="form-control view-limit">
+                        <option value="">View 10</option>
+                        <option value="25">View 25</option>
+                        <option value="50">View 50</option>
+                        <option value="100">View 100</option>
+                    </select>
+                </div>
+            </div>
+            <div class="table-list" id="sortable">
+                <div class="table-list-head">
+                    <div>
+                        <button class="circle-small-warning" type="button" ng-click="list.onClickBulkDeleteAll()"
+                                ng-class="{'active-discount' : list.deleteAll}">
+                        </button>
+                    </div>
+                    <div>Icon</div>
+                    <div>Category Label</div>
+                    <div>Category Name</div>
+                    <div>Visibility</div>
+                    <div>Reorder</div>
+                </div>
+                <div class="table-row"
+                     dir-paginate="(key, item) in list.items|orderBy:list.sortKey:list.reverse|filter:list.search|itemsPerPage:list.limit"
+                     id="id_{{item.referral_id}}||{{item.sort_index}}">
+                    <div>
+                        <button class="xChoose circle-small-warning" type="button"
+                                ng-class="{'active-discount' : list.deleteSelected(item.referral_id)}"
+                                ng-click="list.onClickBulkDelete(item.referral_id)">
+                        </button>
+                    </div>
+                    <div>
+                        <img ng-show="item.image_id" width="70" height="70"
+                             ng-src="<?php echo root_url(), 'root_images/'; ?>{{item.file_dir}}r100_{{item.file_name}}">
+                        <img ng-hide="item.image_id" width="70" height="70"
+                             ng-src="<?php echo $curModule->file_url; ?>icon/image_upload.png">
+                    </div>
+                    <div>
+                        <div class="item-label"
+                             style="background-color:{{item.label_color}}; color: {{item.font_color}}">
+                            {{item.name}}
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <a class="btn-expand" ng-click="list.onClickExpand('lv2', item.referral_id, $event)">
+                                <span></span>
+                            </a>
+
+                            <div class="item-name">{{item.name}} ({{item.cate_child_count}})</div>
+                            <a class="btn-add-s" ng-click="list.createSubCate(item.referral_id, item.name)">
+                                <span></span>
+                            </a>
+                            <a class="btn-edit" id="btn-edit-{{item.referral_id}}"
+                               ng-click="list.onClickEdit(item.referral_id)"
+                               data-referral_id="{{item.referral_id}}"
+                               data-name="{{item.name}}"
+                               data-image_id="{{item.image_id}}"
+                               data-image="<?php echo root_url(), 'root_images/'; ?>{{item.file_dir}}r100_{{item.file_name}}"
+                               data-label="{{item.label_color}}"
+                               data-font="{{item.font_color}}">
+                                <span></span>
+                            </a>
+                            <a class="btn-del-s" ng-click="list.setStatusBlock(item.referral_id, 0)">
+                                <span></span>
+                            </a>
+                        </div>
+                        <div class="clear"></div>
+                        <div class="cate-lv2-box hide" id="cate-lv2-box-{{item.referral_id}}">
+                            <!-- lv2 -->
+                            <div ng-repeat="(key2, v2) in item.cate_child" class="cate-lv-2">
+                                <div class="clear"></div>
+                                <a class="btn-expand" ng-click="list.onClickExpand('lv3', v2.referral_id, $event)">
+                                    <span></span>
+                                </a>
+
+                                <div class="item-name">{{v2.name}} ({{v2.cate_child_count}})</div>
+                                <a class="btn-add-s" ng-click="list.createSubCate(v2.referral_id, v2.name)">
+                                    <span></span>
+                                </a>
+                                <a class="btn-edit" ng-click="list.onClickEditSubCate(v2.referral_id, v2.name)">
+                                    <span></span>
+                                </a>
+                                <a class="btn-del-s" ng-click="list.setStatusBlock(v2.referral_id, 0)">
+                                    <span></span>
+                                </a>
+
+                                <div class="clear"></div>
+                                <!-- lv3 -->
+                                <div ng-repeat="(key3, v3) in v2.cate_child" class="cate-lv-3 hide"
+                                     id="cate-lv3-box-{{v2.referral_id}}">
+                                    <div class="clear"></div>
+                                    <a class="btn-expand" ng-click="list.onClickExpand('lv4', v3.referral_id, $event)">
+                                        <span></span>
+                                    </a>
+
+                                    <div class="item-name">{{v3.name}} ({{v3.cate_child_count}})</div>
+                                    <a class="btn-add-s" ng-click="list.createSubCate(v3.referral_id, v3.name)">
+                                        <span></span>
+                                    </a>
+                                    <a class="btn-edit" ng-click="list.onClickEditSubCate(v3.referral_id, v3.name)">
+                                        <span></span>
+                                    </a>
+                                    <a class="btn-del-s" ng-click="list.setStatusBlock(v3.referral_id, 0)">
+                                        <span></span>
+                                    </a>
+
+                                    <div class="clear"></div>
+                                    <!-- lv4 -->
+                                    <div ng-repeat="(key4, v4) in v3.cate_child" class="cate-lv-4 hide"
+                                         id="cate-lv4-box-{{v3.referral_id}}">
+                                        <div class="clear"></div>
+                                        <div class="item-name">{{v4.name}} ({{v4.cate_child_count}})</div>
+                                        <a class="btn-add-s" ng-click="list.createSubCate(v4.referral_id, v4.name)">
+                                            <span></span>
+                                        </a>
+                                        <a class="btn-edit" ng-click="list.onClickEditSubCate(v4.referral_id, v4.name)">
+                                            <span></span>
+                                        </a>
+                                        <a class="btn-del-s" ng-click="list.setStatusBlock(v4.referral_id, 0)">
+                                            <span></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <a ng-show="item.status == 1"
+                           ng-click="list.setStatusBlock(item.referral_id, 2)"
+                           class="item-show">
+                            Show
+                        </a>
+                        <a ng-show="item.status == 2"
+                           ng-click="list.setStatusBlock(item.referral_id, 1)"
+                           class="item-hide">
+                            hide
+                        </a>
+                    </div>
+                    <div><a class="btn-re-oder"><span></span></a></div>
+                    <script type="text/javascript">
+                        $(document).ready(function () {
+                            $(".btn-expand").click(function () {
+                                if ($(this).attr("class") != 'btn-expand') {
+                                    $(this).removeClass("btn-expand");
+                                    $(this).addClass("btn-expand-up");
+                                } else {
+                                    $(this).removeClass("btn-expand-up");
+                                    $(this).addClass("btn-expand");
+                                }
+                            });
+                        });
+                    </script>
+                </div>
+            </div>
+            <div class="row" ng-show="list.items">
+                <div class="col-xs-6">
+                    <dir-pagination-controls
+                        template-url="<?php echo $curModule->file_url; ?>template/summary-pagination.html">
+                    </dir-pagination-controls>
+                </div>
+                <div class="col-xs-6">
+                    <div class="dataTables_paginate paging_bootstrap">
+                        <dir-pagination-controls
+                            max-size="5"
+                            direction-links="true"
+                            boundary-links="true">
+                        </dir-pagination-controls>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div ng-hide="list.items" class="clearfix row no-customer">
+                <div class="col-xs-12">
+                    <img ng-src="<?php echo $curModule->file_url; ?>icon/no_item.png">
+
+                    <div>ยังไม่มีหมวดสินค้า ทำการเพิ่มหมวดสินค้าได้ที่ด้านบน</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ root_modules/com_gae_product_attribute/v1_0_0/staticfiles/js/list/list.controller.js
@@ -0,0 +1,326 @@
+(function() {
+  'use strict';
+
+  angular
+      .module('category')
+      .controller('ListController', ListController);
+
+  function ListController($scope, $timeout, $cookies, $window) {
+    var vm = this,
+        cateId,
+        notification,
+        apiUrl,
+        successMessage,
+        errorMessage;
+    vm.items = [];
+    vm.total = 0;
+    vm.limit = 10;
+    vm.selectedDeleteId = [];
+    vm.deleteAll = false;
+    vm.onClickBulkDeleteAll = onClickBulkDeleteAll;
+    vm.imageProfile = '';
+    vm.placeholderSubCateName = '';
+    vm.clickOnUpload = clickOnUpload;
+    vm.addCategory = addCategory;
+    vm.displaySubCateForm = false;
+    vm.onChangeLimit = function (limitList) {
+      vm.limit = limitList || 10;
+    };
+    vm.onChangeBulkDelete = function (action) {
+      onChangeBulkDelete(action);
+    };
+    vm.onClickSort = function (keyname) {
+      onClickSort(keyname);
+    };
+    vm.onClickBulkDelete = function (id) {
+      onClickBulkDelete(id);
+    };
+    vm.deleteSelected = function (id) {
+      return vm.selectedDeleteId.indexOf(id) > -1;
+    };
+    vm.setStatusBlock = function (id, status) {
+      setStatusBlock(id, status);
+    };
+    vm.keyDownRequired = function ($event) {
+      keyDownRequired($event);
+    };
+    vm.onClickEdit = function (id) {
+      onClickEdit(id);
+    };
+    vm.createSubCate = function (id, name) {
+      createSubCate(id, name);
+    };
+    vm.onClickEditSubCate = function (element) {
+      onClickEditSubCate(element);
+    };
+    vm.onClickExpand = function (lv, id, $event) {
+      onClickExpand(lv, id, $event);
+    };
+
+    function onClickExpand(lv, id, $event) {
+      var element = angular.element($event.target).parent();
+      if (element.hasClass('btn-expand')) {
+        element.removeClass('btn-expand');
+        element.addClass('btn-expand-up');
+        angular.element('#cate-' + lv + '-box-' + id).removeClass('hide');
+      } else {
+        element.removeClass('btn-expand-up');
+        element.addClass('btn-expand');
+        angular.element('#cate-' + lv + '-box-' + id).addClass('hide');
+      }
+    }
+
+    function onClickEditSubCate(id, name) {
+      vm.displaySubCateForm = true;
+      cateId = id;
+      if (name) {
+        vm.subCategoryName = name;
+      }
+      angular.element('.btn-add').addClass('btn-save');
+      angular.element('.btn-add').removeClass('btn-add');
+    }
+
+    function createSubCate(id, name) {
+      if (!vm.displaySubCateForm) {
+        vm.displaySubCateForm = true;
+      }
+      vm.parentId = id;
+      vm.placeholderSubCateName = name;
+    }
+
+    function onClickEdit(id) {
+      vm.displaySubCateForm = false;
+      cateId = id;
+      angular.element('#pic-icon').removeClass('ng-hide');
+      angular.element('#colorPicker .colorInner').removeAttr("style");
+      angular.element('#colorPicker2 .colorInner').removeAttr("style");
+      var element = '#btn-edit-' + id;
+      var name = angular.element(element).data('name');
+      var imageId = angular.element(element).data('image_id');
+      var image = angular.element(element).data('image');
+      var label = angular.element(element).data('label');
+      var font = angular.element(element).data('font');
+      if (name) {
+        vm.categoryName = angular.element(element).data('name');
+      }
+      if (imageId) {
+        vm.imageProfile = angular.element(element).data('image');
+      }
+      if (label !== '#eee') {
+        angular.element('#colorPicker .colorInner').css('background', label);
+      }
+      if (font !== '#666') {
+        angular.element('#colorPicker2 .colorInner').css('background', font);
+      }
+      angular.element('.btn-add').addClass('btn-save');
+      angular.element('.btn-add').removeClass('btn-add');
+    }
+
+    notification = $cookies.getObject('cus_list_noti');
+    if (notification && notification.time == getUrlParameter('timestamp')) {
+      GAEUI.notification().playComplete(notification.message);
+      $cookies.remove('cus_list_noti', {'path': '/'});
+    }
+
+    fetchListing();
+
+    function fetchListing() {
+      CUR_MODULE.apiGet("start/listing").then(function (res) {
+        $scope.$apply(function () {
+          vm.items = res.data.items;
+          vm.total = res.data.items.length;
+          vm.sortIndex = ++res.data.order;
+        });
+      });
+    }
+
+    function onChangeBulkDelete(action) {
+      GAEUI.confirmBox().play("Are you sure?","please confirm your action.",function(bool){
+        if(bool){
+          GAEUI.confirmBox().stop();
+          GAEUI.pageLoading().play();
+          if (vm.selectedDeleteId.length === 0) {
+            vm.bulkDelete = "";
+            GAEUI.pageLoading().stop();
+            GAEUI.notification().playError('Please select some category');
+          } else {
+            if (action == 1) {
+              apiUrl = 'start/bulk_show';
+              successMessage = 'Update status category complete';
+              errorMessage = 'Can not update status category';
+            } else if (action == 2) {
+              apiUrl = 'start/bulk_hide';
+              successMessage = 'Update status category complete';
+              errorMessage = 'Can not update status category';
+            } else if (action == 3) {
+              apiUrl = 'start/bulk_delete';
+              successMessage = 'Delete category complete';
+              errorMessage = 'Can not delete category';
+            }
+            var dataSend = {
+              "ids": vm.selectedDeleteId
+            };
+            CUR_MODULE.apiPost(apiUrl, dataSend).then(function (res) {
+              vm.selectedDeleteId = [];
+              vm.bulkDelete = "";
+              if (res.ok) {
+                fetchListing();
+                GAEUI.pageLoading().stop();
+                GAEUI.notification().playComplete(successMessage);
+              } else {
+                GAEUI.pageLoading().stop();
+                GAEUI.notification().playError(errorMessage);
+              }
+            });
+          }
+        } else {
+          vm.selectedDeleteId = [];
+          vm.bulkDelete = "";
+        }
+        GAEUI.confirmBox().stop();
+      },"Done");
+    }
+
+    function onClickSort(keyname) {
+      vm.sortKey = keyname || 'create_time';
+      if (vm.reverse) {
+        vm.reverse = false;
+      } else {
+        vm.reverse = true;
+      }
+    }
+
+    function onClickBulkDeleteAll() {
+      if (vm.deleteAll) {
+        vm.deleteAll = false;
+        vm.selectedDeleteId = [];
+      } else {
+        vm.deleteAll = true;
+        vm.selectedDeleteId = [];
+        angular.forEach(vm.items, function (value, key) {
+          vm.selectedDeleteId.push(value.referral_id);
+        });
+      }
+    }
+
+    function onClickBulkDelete(id) {
+      if (vm.selectedDeleteId.indexOf(id) > -1) {
+        vm.selectedDeleteId.splice(vm.selectedDeleteId.indexOf(id), 1);
+      } else {
+        vm.selectedDeleteId.push(id);
+      }
+    }
+
+    function setStatusBlock(id, status) {
+      GAEUI.pageLoading().play();
+      var dataSend = {
+        "id": id,
+        "status": status
+      };
+      console.log(dataSend);
+      CUR_MODULE.apiPost('start/update_status', dataSend).then(function (res) {
+        if (res.ok) {
+          fetchListing();
+          GAEUI.pageLoading().stop();
+          GAEUI.notification().playComplete("Update status complete");
+        } else {
+          GAEUI.pageLoading().stop();
+          GAEUI.notification().playError('Cannot update status');
+        }
+      });
+    }
+
+    function clickOnUpload() {
+      $timeout(function () {
+        angular.element('#imageCategory').trigger('click');
+      }, 100);
+    }
+
+    function keyDownRequired($event) {
+      angular.element('#' + $event.currentTarget.id).parent().removeClass('has-error');
+    }
+
+    function addCategory(isSubCate) {
+      GAEUI.pageLoading().play();
+      if (cateId) {
+        apiUrl = 'start/update';
+        successMessage = 'Update category complete';
+        errorMessage = 'Can not update category';
+      } else {
+        apiUrl = 'start/add';
+        successMessage = 'Create category complete';
+        errorMessage = 'Can not create category';
+      }
+      if (isSubCate) {
+        var dataSend = {
+          "id": cateId || '',
+          "category_name": vm.subCategoryName || '',
+          "parent_id": vm.parentId || ''
+        };
+      } else {
+        var dataSend = {
+          "id": cateId || '',
+          "category_name": vm.categoryName || '',
+          "parent_id": vm.parentId || '',
+          "sort_index": vm.sortIndex || '',
+          "profile_pic": vm.fileModel,
+          "label_color": angular.element('#labelColor').val() || '',
+          "font_color": angular.element('#fontColor').val() || ''
+        };
+      }
+      CUR_MODULE.apiPost(apiUrl, dataSend).then(function (res) {
+        if (res.ok) {
+          GAEUI.pageLoading().stop();
+          $cookies.putObject('cus_list_noti', {'message': successMessage, 'time':res.data.time}, {'path': '/'});
+          $window.location.href = CUR_MODULE.data.app_url + 'start?timestamp=' + res.data.time;
+        } else {
+          angular.forEach(res.data, function (value, key) {
+            if (value === 'required') {
+              angular.element('#' + key).parent().addClass('has-error');
+              angular.element('#' + key).next().text('ห้ามเว้นว่าง');
+              angular.element('#' + key).next().removeClass('hide');
+            }
+          });
+          GAEUI.pageLoading().stop();
+          GAEUI.notification().playError(errorMessage);
+          vm.parentId = '';
+        }
+      });
+    }
+
+    $(document).ready(function () {
+      var sort;
+      $("#sortable").sortable({
+        handle: ".btn-re-oder",
+        start: function( event, ui ) {
+          sort = $( "#sortable" ).sortable( "serialize", { key: "id" } );
+        },
+        update: function (event, ui) {
+          var dataSend = {
+            'sort': sort,
+            'sorted': $( "#sortable" ).sortable( "serialize", { key: "id" } )
+          };
+          CUR_MODULE.apiPost('start/update_sort', dataSend).then(function (res) {
+            if (res.ok) {
+              fetchListing();
+              GAEUI.notification().playComplete("Update sort complete");
+            } else {
+              GAEUI.notification().playError('Cannot update sort');
+            }
+          });
+        }
+      });
+    });
+
+  }
+
+})();
+function PreviewImage() {
+  var oFReader = new FileReader();
+  oFReader.readAsDataURL(document.getElementById("imageCategory").files[0]);
+  oFReader.onload = function (oFREvent) {
+    document.getElementById("area-inner-image").src = oFREvent.target.result;
+    document.getElementById("area-inner-image").setAttribute('style', 'width:auto !important;');
+    document.getElementById("pic-icon").style.display = "none";
+  };
+}
